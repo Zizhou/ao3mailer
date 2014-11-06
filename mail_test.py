a7bec7d583b0a71edca565498538dbf545c401c7
Daniel <dan.t.ching@gmail.com>
2014-11-05 20:59:51 -0800

    oops

--- mail_test.py
+++ mail_test.py
@@ -1,18 +1,22 @@
 import mailbot
 
+###REPLACE THIS!!
+mail_to = 'mail@mail.mail'
+###
+
 def run_test():
     mobi_file = mailbot.get_file('http://archiveofourown.org/works/829218?view_adult=true')
 
     print mobi_file
     print "now testing MIME packing"
-    mime = mailbot.pack_MIME('givemeviruses@gmail.com', 'http://archiveofourown.org/works/829218?view_adult=true')
+    mime = mailbot.pack_MIME(mail_to, 'http://archiveofourown.org/works/829218?view_adult=true')
 
     print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
     print 'now sending...'
-    mailbot.send_mail(mime, 'givemeviruses@gmail.com')
+    mailbot.send_mail(mime, mail_to)
     print 'done!'
 
 def run_diagnostic():
-    mail = mailbot.diagnostic('I am the very model of a modern major general','givemeviruses@gmail.com')
+    mail = mailbot.diagnostic('I am the very model of a modern major general',mail_to)
 
-    mailbot.send_mail(mail, 'givemeviruses@gmail.com')
+    mailbot.send_mail(mail, mail_to)
